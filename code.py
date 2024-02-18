2ce9ed8dd1fe08789f466bb135538cfbda09d7ac
felirx <felirx@gmail.com>
2024-02-18 15:59:20 +0200

    Holdable numpad layer to left thumb cluster

--- code.py
+++ code.py
@@ -19,6 +19,7 @@ XXXXXXX = KC.NO
 RAISE = KC.MO(3)
 MEDIAS = KC.TG(4)
 LOWER = KC.TG(2)
+LOWEH = KC.MO(2)
 TODVORK = KC.DF(1)
 TOQWERT = KC.DF(0)
 
@@ -36,21 +37,21 @@ keyboard.extensions.append(MediaKeys())
 keyboard.keymap = [
     [ #QWERTY
         KC.F12,   KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,      KC.F6,    KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,\
-        KC.GRAVE, KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,      KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.LBRC,\
-        KC.TAB,   KC.Q,     KC.W,     KC.E,     KC.R,     KC.T,       KC.Y,     KC.U,     KC.I,     KC.O,     KC.P,     KC.RBRC,\
+        KC.GRAVE, KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,      KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.RBRC,\
+        KC.TAB,   KC.Q,     KC.W,     KC.E,     KC.R,     KC.T,       KC.Y,     KC.U,     KC.I,     KC.O,     KC.P,     KC.LBRC,\
         KC.ESC,   KC.A,     KC.S,     KC.D,     KC.F,     KC.G,       KC.H,     KC.J,     KC.K,     KC.L,     KC.SCLN,  KC.QUOT,\
         KC.LSFT,  KC.Z,     KC.X,     KC.C,     KC.V,     KC.B,       KC.N,     KC.M,     KC.COMM,  KC.DOT,   KC.SLSH,  KC.RSFT,\
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LCTL,  KC.SPC,     KC.BSPC,  KC.ENT,   XXXXXXX,  KC.MINUS, KC.EQUAL, KC.RCTL,\
-                            XXXXXXX,  KC.LGUI,  RAISE,    KC.LALT,    KC.RALT,  LOWER,    KC.DEL,   XXXXXXX,
+                            LOWEH,    KC.LGUI,  RAISE,    KC.LALT,    KC.RALT,  LOWER,    KC.DEL,   XXXXXXX,
     ],
     [ #backup dvorak if no sys support
         KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,    KC.F6,      KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,   KC.F12,\
-        KC.GRAVE, KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,      KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.SLSH,\
-        KC.TAB,   KC.QUOT,  KC.COMM,  KC.DOT,   KC.P,     KC.Y,       KC.F,     KC.G,     KC.C,     KC.R,     KC.L,     KC.EQUAL,\
+        KC.GRAVE, KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,      KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.EQUAL,\
+        KC.TAB,   KC.QUOT,  KC.COMM,  KC.DOT,   KC.P,     KC.Y,       KC.F,     KC.G,     KC.C,     KC.R,     KC.L,     KC.SLASH,\
         KC.ESC,   KC.A,     KC.O,     KC.E,     KC.U,     KC.I,       KC.D,     KC.H,     KC.T,     KC.N,     KC.S,     KC.MINS,\
         KC.LSFT,  KC.SCLN,  KC.Q,     KC.J,     KC.K,     KC.X,       KC.B,     KC.M,     KC.W,     KC.V,     KC.Z,     KC.RSFT,\
         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LCTL,  KC.SPC,     KC.BSPC,  KC.ENT,   XXXXXXX,  KC.LBRC,  KC.RBRC,  KC.RCTL,\
-                            XXXXXXX,  KC.LGUI,  RAISE,    KC.LALT,    KC.RALT,  LOWER,    KC.DEL,   XXXXXXX
+                            LOWEH,    KC.LGUI,  RAISE,    KC.LALT,    KC.RALT,  LOWER,    KC.DEL,   XXXXXXX
      ],
     [ #numpad, mediakeys, arrows, stuff
         _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,\
