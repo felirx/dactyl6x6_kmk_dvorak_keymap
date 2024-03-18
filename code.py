35e372a94f42f43e9620f6f6efcbc4ee5c4cb427
felirx <felirx@gmail.com>
2024-03-18 15:25:52 +0200

    cosmotyl binds

--- code.py
+++ code.py
@@ -41,17 +41,17 @@ keyboard.keymap = [
         KC.TAB,   KC.Q,     KC.W,     KC.E,     KC.R,     KC.T,       KC.Y,     KC.U,     KC.I,     KC.O,     KC.P,     KC.LBRC,\
         KC.ESC,   KC.A,     KC.S,     KC.D,     KC.F,     KC.G,       KC.H,     KC.J,     KC.K,     KC.L,     KC.SCLN,  KC.QUOT,\
         KC.LSFT,  KC.Z,     KC.X,     KC.C,     KC.V,     KC.B,       KC.N,     KC.M,     KC.COMM,  KC.DOT,   KC.SLSH,  KC.RSFT,\
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LALT,  KC.LCTL,  KC.SPC,     KC.BSPC,  LOWEH,    KC.RALT,  KC.MINUS, KC.EQUAL, KC.RCTL,\
-                            LOWEH,    XXXXXXX,  RAISE,    KC.LSFT,    KC.RSFT,  XXXXXXX,  KC.DEL,   KC.LGUI,
-    ],
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LALT,  KC.LCTL,  KC.SPC,     XXXXXXX,  KC.DEL,   KC.RALT,  KC.MINUS, KC.EQUAL, KC.RCTL,\
+                            LOWEH,    XXXXXXX,  RAISE,    KC.LSFT,    KC.LGUI,  KC.RSFT,  KC.BSPC,  LOWEH
+     ],
     [ #backup dvorak if no sys support
         KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,    KC.F6,      KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,   KC.F12,\
         KC.GRAVE, KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,      KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.EQUAL,\
         KC.TAB,   KC.QUOT,  KC.COMM,  KC.DOT,   KC.P,     KC.Y,       KC.F,     KC.G,     KC.C,     KC.R,     KC.L,     KC.SLASH,\
         KC.ESC,   KC.A,     KC.O,     KC.E,     KC.U,     KC.I,       KC.D,     KC.H,     KC.T,     KC.N,     KC.S,     KC.MINS,\
         KC.LSFT,  KC.SCLN,  KC.Q,     KC.J,     KC.K,     KC.X,       KC.B,     KC.M,     KC.W,     KC.V,     KC.Z,     KC.RSFT,\
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LALT,  KC.LCTL,  KC.SPC,     KC.BSPC,  LOWEH,    KC.RALT,  KC.LBRC,  KC.RBRC,  KC.RCTL,\
-                            LOWEH,    XXXXXXX,  RAISE,    KC.LSFT,    KC.RSFT,  XXXXXXX,  KC.DEL,   KC.LGUI
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LALT,  KC.LCTL,  KC.SPC,     XXXXXXX,  KC.DEL,   KC.RALT,  KC.LBRC,  KC.RBRC,  KC.RCTL,\
+                            LOWEH,    XXXXXXX,  RAISE,    KC.LSFT,    KC.LGUI,  KC.RSFT,  KC.BSPC,  LOWEH
      ],
     [ #numpad, mediakeys, arrows, stuff
         _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,\
@@ -66,19 +66,19 @@ keyboard.keymap = [
         _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,\
         XXXXXXX,  KC.INS,   KC.PSCR,  XXXXXXX,  KC.PAUS,  KC.HOME,    TODVORK,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
         _______,  KC.CAPS,  XXXXXXX,  KC.UP,    XXXXXXX,  KC.PGUP,    TOQWERT,  XXXXXXX,  KC.LCBR , KC.RCBR,  KC.BSLS,  KC.BSLS,\
-        _______,  XXXXXXX,  KC.LEFT,  KC.DOWN,  KC.RGHT,  KC.PGDN,    XXXXXXX,  XXXXXXX,  KC.LBRC,  KC.RBRC,  KC.PIPE,  KC.PIPE,\
+        _______,  XXXXXXX,  KC.LEFT,  KC.DOWN,  KC.RGHT,  KC.PGDN,    MEDIAS,   XXXXXXX,  KC.LBRC,  KC.RBRC,  KC.PIPE,  KC.PIPE,\
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.END,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,    _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
                             _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______
     ],
     [ #mediakeys and memes
         _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,\
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX , XXXXXXX,  XXXXXXX,  XXXXXXX,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,    _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
-                            _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  MEDIAS,     KC.N6,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    KC.Y,     XXXXXXX,  XXXXXXX , XXXXXXX,  XXXXXXX,  XXXXXXX,\
+        _______,  KC.A,     KC.S,     KC.D,     KC.F,     XXXXXXX,    XXXXXXX,  KC.J,     KC.K,     KC.L,     KC.SCLN,  XXXXXXX,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  KC.M,     KC.COMM,  KC.DOT,   XXXXXXX,  _______,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,    KC.Z,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
+                            XXXXXXX,  _______,  XXXXXXX,  _______,    _______,  _______,  _______,  _______
     ]
 ]
 
