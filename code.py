b5742b6b53e3e110e638edfcfec94bc2410a133c
felirx <felirx@gmail.com>
2024-04-17 14:57:05 +0300

    Cosmotyl with 5thumb and 3 extras layout for left side

--- code.py
+++ code.py
@@ -36,49 +36,49 @@ keyboard.extensions.append(MediaKeys())
 
 keyboard.keymap = [
     [ #backup dvorak if no sys support
-        KC.F12,   KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,      KC.F6,    KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,\
-        KC.GRAVE, KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,      KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.EQUAL,\
-        KC.TAB,   KC.QUOT,  KC.COMM,  KC.DOT,   KC.P,     KC.Y,       KC.F,     KC.G,     KC.C,     KC.R,     KC.L,     KC.SLASH,\
-        KC.ESC,   KC.A,     KC.O,     KC.E,     KC.U,     KC.I,       KC.D,     KC.H,     KC.T,     KC.N,     KC.S,     KC.MINS,\
-        KC.LSFT,  KC.SCLN,  KC.Q,     KC.J,     KC.K,     KC.X,       KC.B,     KC.M,     KC.W,     KC.V,     KC.Z,     KC.RSFT,\
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LALT,  XXXXXXX,  XXXXXXX,    LOWER,    KC.DEL,   KC.RALT,  XXXXXXX,  XXXXXXX,  KC.RCTL,\
-                            KC.LCTL,  KC.SPC,   KC.LSFT,  KC.LGUI,    RAISE,    KC.RSFT,  KC.BSPC,  LOWEH
+        KC.F12,   KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,                          KC.F6,    KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,\
+        KC.GRAVE, KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,    KC.LGUI,    XXXXXXX,  KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.EQUAL,\
+        KC.TAB,   KC.QUOT,  KC.COMM,  KC.DOT,   KC.P,     KC.Y,     XXXXXXX,    XXXXXXX,  KC.F,     KC.G,     KC.C,     KC.R,     KC.L,     KC.SLASH,\
+        KC.ESC,   KC.A,     KC.O,     KC.E,     KC.U,     KC.I,     XXXXXXX,    XXXXXXX,  KC.D,     KC.H,     KC.T,     KC.N,     KC.S,     KC.MINS,\
+        KC.LSFT,  KC.SCLN,  KC.Q,     KC.J,     KC.K,     KC.X,                           KC.B,     KC.M,     KC.W,     KC.V,     KC.Z,     KC.RSFT,\
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LALT,  KC.LCTL,  KC.SPC,                         LOWER,    KC.DEL,   KC.RALT,  XXXXXXX,  XXXXXXX,  KC.RCTL,\
+                            XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LSFT,                        RAISE,    KC.RSFT,  KC.BSPC,  LOWEH
      ],
     [ #QWERTY
-        KC.F12,   KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,      KC.F6,    KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,\
-        KC.GRAVE, KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,      KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.RBRC,\
-        KC.TAB,   KC.Q,     KC.W,     KC.E,     KC.R,     KC.T,       KC.Y,     KC.U,     KC.I,     KC.O,     KC.P,     KC.LBRC,\
-        KC.ESC,   KC.A,     KC.S,     KC.D,     KC.F,     KC.G,       KC.H,     KC.J,     KC.K,     KC.L,     KC.SCLN,  KC.QUOT,\
-        KC.LSFT,  KC.Z,     KC.X,     KC.C,     KC.V,     KC.B,       KC.N,     KC.M,     KC.COMM,  KC.DOT,   KC.SLSH,  KC.RSFT,\
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LALT,  XXXXXXX,  XXXXXXX,    LOWER,    KC.DEL,   KC.RALT,  XXXXXXX,  XXXXXXX,  KC.RCTL,\
-                            KC.LCTL,  KC.SPC,   KC.LSFT,  KC.LGUI,    RAISE,    KC.RSFT,  KC.BSPC,  LOWEH
+        KC.F12,   KC.F1,    KC.F2,    KC.F3,    KC.F4,    KC.F5,                          KC.F6,    KC.F7,    KC.F8,    KC.F9,    KC.F10,   KC.F11,\
+        KC.GRAVE, KC.N1,    KC.N2,    KC.N3,    KC.N4,    KC.N5,    KC.LGUI,    XXXXXXX,  KC.N6,    KC.N7,    KC.N8,    KC.N9,    KC.N0,    KC.RBRC,\
+        KC.TAB,   KC.Q,     KC.W,     KC.E,     KC.R,     KC.T,     XXXXXXX,    XXXXXXX,  KC.Y,     KC.U,     KC.I,     KC.O,     KC.P,     KC.LBRC,\
+        KC.ESC,   KC.A,     KC.S,     KC.D,     KC.F,     KC.G,     XXXXXXX,    XXXXXXX,  KC.H,     KC.J,     KC.K,     KC.L,     KC.SCLN,  KC.QUOT,\
+        KC.LSFT,  KC.Z,     KC.X,     KC.C,     KC.V,     KC.B,                           KC.N,     KC.M,     KC.COMM,  KC.DOT,   KC.SLSH,  KC.RSFT,\
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LALT,  KC.LCTL,  KC.SPC,                         LOWER,    KC.DEL,   KC.RALT,  XXXXXXX,  XXXXXXX,  KC.RCTL,\
+                            XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.LSFT,                        RAISE,    KC.RSFT,  KC.BSPC,  LOWEH
      ],
     [ #numpad, mediakeys, arrows, stuff
-        _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,\
-        XXXXXXX,  XXXXXXX,  KC.LPRN,  KC.EQUAL, KC.RPRN,  XXXXXXX,    KC.CAPS,  XXXXXXX,  KC.PSLS,  KC.PAST,  KC.PMNS,  XXXXXXX,\
-        _______,  XXXXXXX,  KC.LBRC,  KC.UP,    KC.RBRC,  XXXXXXX,    XXXXXXX,  KC.P7,    KC.P8,    KC.P9,    KC.PPLS,  XXXXXXX,\
-        _______,  XXXXXXX,  KC.LEFT,  KC.DOWN,  KC.RGHT,  KC.PIPE,    XXXXXXX,  KC.P4,    KC.P5,    KC.P6,    KC.ENT,   XXXXXXX,\
-        _______,  XXXXXXX,  KC.LCBR,  XXXXXXX,  KC.RCBR,  KC.BSLS,    XXXXXXX,  KC.P1,    KC.P2,    KC.P3,    KC.PENT,  _______,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,    _______,  _______,  KC.P0,    KC.PDOT,  KC.PENT,  _______,\
-                            _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______
+        _______,  _______,  _______,  _______,  _______,  _______,                        _______,  _______,  _______,  _______,  _______,  _______,\
+        XXXXXXX,  XXXXXXX,  KC.LPRN,  KC.EQUAL, KC.RPRN,  XXXXXXX,  _______,    _______,  KC.CAPS,  XXXXXXX,  KC.PSLS,  KC.PAST,  KC.PMNS,  XXXXXXX,\
+        _______,  XXXXXXX,  KC.LBRC,  KC.UP,    KC.RBRC,  KC.K,     _______,    _______,  XXXXXXX,  KC.P7,    KC.P8,    KC.P9,    KC.PPLS,  XXXXXXX,\
+        _______,  XXXXXXX,  KC.LEFT,  KC.DOWN,  KC.RGHT,  KC.PIPE,  _______,    _______,  XXXXXXX,  KC.P4,    KC.P5,    KC.P6,    KC.ENT,   XXXXXXX,\
+        _______,  XXXXXXX,  KC.LCBR,  XXXXXXX,  KC.RCBR,  KC.BSLS,                        XXXXXXX,  KC.P1,    KC.P2,    KC.P3,    KC.PENT,  _______,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,                        _______,  _______,  KC.P0,    KC.PDOT,  KC.PENT,  _______,\
+                            _______,  _______,  _______,  _______,                        _______,  _______,  _______,  _______
      ],
     [ #symbols that are missink from 6x6 
-        _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,\
-        XXXXXXX,  KC.INS,   KC.PSCR,  XXXXXXX,  KC.PAUS,  KC.HOME,    TODVORK,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.PGUP,    TOQWERT,  XXXXXXX,  XXXXXXX , XXXXXXX,  XXXXXXX,  XXXXXXX,\
-        _______,  KC.MSTP,  KC.MPRV,  KC.MPLY,  KC.MNXT,  KC.PGDN,    MEMES,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.END,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,    _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
-                            _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______
+        _______,  _______,  _______,  _______,  _______,  _______,                        _______,  _______,  _______,  _______,  _______,  _______,\
+        XXXXXXX,  KC.INS,   KC.PSCR,  XXXXXXX,  KC.PAUS,  KC.HOME,  _______,    _______,  TODVORK,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.PGUP,  _______,    _______,  TOQWERT,  XXXXXXX,  XXXXXXX , XXXXXXX,  XXXXXXX,  XXXXXXX,\
+        _______,  KC.MSTP,  KC.MPRV,  KC.MPLY,  KC.MNXT,  KC.PGDN,  _______,    _______,  MEMES,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  KC.END,                         XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,                        _______,  _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
+                            _______,  _______,  _______,  _______,                        _______,  _______,  _______,  _______
     ],
     [ #memes
-        _______,  _______,  _______,  _______,  _______,  _______,    _______,  _______,  _______,  _______,  _______,  _______,\
-        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  MEMES,      KC.N6,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    KC.Y,     XXXXXXX,  XXXXXXX , XXXXXXX,  XXXXXXX,  XXXXXXX,\
-        _______,  KC.A,     KC.S,     KC.D,     KC.F,     XXXXXXX,    XXXXXXX,  KC.J,     KC.K,     KC.L,     KC.SCLN,  XXXXXXX,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,    XXXXXXX,  KC.M,     KC.COMM,  KC.DOT,   XXXXXXX,  _______,\
-        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,    KC.Z,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
-                            XXXXXXX,  _______,  XXXXXXX,  _______,    _______,  _______,  _______,  _______
+        _______,  _______,  _______,  _______,  _______,  _______,                        _______,  _______,  _______,  _______,  _______,  _______,\
+        XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  MEMES,    _______,    _______,  KC.N6,    XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,    _______,  KC.Y,     XXXXXXX,  XXXXXXX , XXXXXXX,  XXXXXXX,  XXXXXXX,\
+        _______,  KC.A,     KC.S,     KC.D,     KC.F,     XXXXXXX,  _______,    _______,  XXXXXXX,  KC.J,     KC.K,     KC.L,     KC.SCLN,  XXXXXXX,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,                        XXXXXXX,  KC.M,     KC.COMM,  KC.DOT,   XXXXXXX,  _______,\
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,  _______,                        KC.Z,     XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,\
+                            XXXXXXX,  _______,  XXXXXXX,  _______,                        _______,  _______,  _______,  _______
     ]
 ]
 
--- kb.py
+++ kb.py
@@ -3,6 +3,7 @@ import board
 from kmk.kmk_keyboard import KMKKeyboard as _KMKKeyboard
 from kmk.scanners import DiodeOrientation
 
+
 class KMKKeyboard(_KMKKeyboard):
     col_pins = (
             board.GP6,
@@ -10,7 +11,8 @@ class KMKKeyboard(_KMKKeyboard):
             board.GP8,
             board.GP9,
             board.GP10,
-            board.GP11
+            board.GP11,
+            board.GP12,
     )
     row_pins = (
             board.GP16,
@@ -25,11 +27,11 @@ class KMKKeyboard(_KMKKeyboard):
     # flake8: noqa
     # fmt: off
     coord_mapping = [             
-         0,  1,  2,  3,  4,  5,     47, 46, 45, 44, 43, 42,
-         6,  7,  8,  9, 10, 11,     53, 52, 51, 50, 49, 48, 
-        12, 13, 14, 15, 16, 17,     59, 58, 57, 56, 55, 54,
-        18, 19, 20, 21, 22, 23,     65, 64, 63, 62, 61, 60,
-        24, 25, 26, 27, 28, 29,     71, 70, 69, 68, 67, 66,
-        30, 31, 32, 33, 34, 35,     77, 76, 75, 74, 73, 72,
-                38, 39, 40, 41,     83, 82, 81, 80
+         0,  1,  2,  3,  4,  5,             54, 53, 52, 51, 50, 49,
+         7,  8,  9, 10, 11, 12, 13,     62, 61, 60, 59, 58, 57, 56,
+        14, 15, 16, 17, 18, 19, 20,     69, 68, 67, 66, 65, 64, 63,
+        21, 22, 23, 24, 25, 26, 27,     76, 75, 74, 73, 72, 71, 70,
+        28, 29, 30, 31, 32, 33,             82, 81, 80, 79, 78, 77,
+        35, 36, 37, 38, 39, 40,             89, 88, 87, 86, 85, 84,
+                44, 45, 46, 47,             96, 95, 94, 93
     ]  
